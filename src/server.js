9764946ebf7276d39ea6776e3502ff837760cdfb
hadeel_salamin <hadeel_salamin@hotmail.com>
2018-11-27 22:19:11 +0200

    hosting my app on heroku #15

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const http = require('http');
-const PORT = 3000;
 const router = require('./router.js');
+require('dotenv').config()
+
+const PORT = process.env.PORT || 3000;
+
 const server = http.createServer(router)
 server.listen(PORT,()=>{
   console.log(` the server up and running on port ${PORT}!`);
